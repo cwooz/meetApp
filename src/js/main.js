67b8a13e298e8013e62e079973caaca72a8e510f
cwooz <woosley.christopher@gmail.com>
2018-03-16 15:12:16 -0400

    js

--- src/js/main.js
+++ src/js/main.js
@@ -5,3 +5,12 @@ document.addEventListener('DOMContentLoaded', function () {
   let skew = 0;
   let delta = 0;
 });
+
+if(wrapper.className.indexOf('skewed') != -1){
+  skew = 1000;
+}
+
+wrapper.addEventListener('mousemove', function(e){
+  //delta = (e.clientX);
+  console.log(e.clientX);
+});
