b1f5398d2fb6f6dd5a16aa9aeb83f794932125f9
cwooz <woosley.christopher@gmail.com>
2018-03-16 15:09:49 -0400

    js

---
+++ src/js/main.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function () {
+  let wrapper = document.getElementById('wrapper');
+  let topLayer = wrapper.querySelector('.top');
+  let handle = wrapper.querySelector('.handle');
+  let skew = 0;
+  let delta = 0;
+});
